a7d68f4ec6f706b8f312d36b028fa05ec38a4329
Doughty <russell.doughty@ou.edu>
2022-07-11 16:54:43 -0500

    Linux version

--- clip_nc.R
+++ clip_nc.R
@@ -1,71 +1,101 @@
 library(terra)
 library(ncdf4)
+library(parallel)
 
-in_dir    <- "G:/TROPOMI/esa/original/v2.1/l2b/2019"
-roi_file  <- "G:/Africa/Ghana/Ghana_Disturbance_Data/Ghana_Protected_Reserves.shp"
-out_dir   <- "G:/TROPOMI/esa/extracted/Ghana/protected_reserves"
+terraOptions(memfrac = 0.8) # Fraction of memory to allow terra
 
-clip_TROPOSIF <- function (in_dir, roi_file, out_dir){
+tmpdir    <- "/mnt/c/Rwork"
+roi_file  <- "/mnt/g/Africa/Ghana/Ghana_Disturbance_Data/Ghana_Protected_Reserves.shp"
+out_dir   <- "/mnt/g/TROPOMI/esa/extracted/Ghana/protected_reserves"
+f_list    <- list.files("/mnt/g/TROPOMI/esa/original/v2.1/l2b/2019", pattern = "*.nc", full.names = TRUE, recursive = TRUE)
+
+# in_dir    <- "G:/TROPOMI/esa/original/v2.1/l2b/2019"
+# roi_file  <- "G:/Africa/Ghana/Ghana_Disturbance_Data/Ghana_Protected_Reserves.shp"
+# out_dir   <- "G:/TROPOMI/esa/extracted/Ghana/protected_reserves"
+
+tmp_create <- function(tmpdir) {
+  
+  p_tmp_dir <- paste0(tmpdir, "/", as.character(Sys.getpid())) # Process ID
+  
+  if (!dir.exists(p_tmp_dir)) {
+    dir.create(p_tmp_dir, recursive = TRUE)
+  }
+  
+  terraOptions(tempdir = p_tmp_dir)
+}
+tmp_remove <- function(tmpdir) {
+  
+  p_tmp_dir <- paste0(tmpdir, "/", as.character(Sys.getpid())) # Process ID
+  unlink(p_tmp_dir, recursive = TRUE)
+}
+
+clip_TROPOSIF <- function (input_file, roi_file, out_dir, tmpdir){
+  
+  tmp_create(tmpdir)
   
   if (!dir.exists(out_dir)){
     dir.create(out_dir, recursive = TRUE)
   }
   
-  # List tropo files
-  f_list <- list.files(in_dir, pattern = "*.nc", full.names = TRUE, recursive = TRUE)
-  
   # vectorize roi shp file for clipping
   roi <- vect(roi_file)
   
-  for (i in 1:length(f_list)) {
-    
-    time_s <- Sys.time()
-    
-    print()
-    t_data <- nc_open(f_list[i])
-    
-    # Get spatial and time
-    lon <- ncvar_get(t_data, "PRODUCT/longitude")
-    lat <- ncvar_get(t_data, "PRODUCT/latitude")
-    t   <- basename(f_list[i])
-    t   <- substr(t, 14, 23)
-    
-    # Get variables
-    ndvi   <- data.frame(NDVI = ncvar_get(t_data, "PRODUCT/NDVI"))
-    nirv   <- data.frame(NIRv = ncvar_get(t_data, "PRODUCT/NIRv"))
-    nirv_r <- data.frame(NIRv_RAD = ncvar_get(t_data, "PRODUCT/NIRv_RAD"))
-    sif    <- data.frame(SIF_743 = ncvar_get(t_data, "PRODUCT/SIF_743"))
-    sif_d  <- data.frame(SIF_Corr_743 = ncvar_get(t_data, "PRODUCT/SIF_Corr_743"))
-    sif_e  <- data.frame(SIF_ERROR_743 = ncvar_get(t_data, "PRODUCT/SIF_ERROR_743"))
-    rad    <- data.frame(Mean_TOA_RAD_743 = ncvar_get(t_data, "PRODUCT/SUPPORT_DATA/DETAILED_RESULTS/Mean_TOA_RAD_743"))
-    pa     <- data.frame(phase_angle = ncvar_get(t_data, "PRODUCT/SUPPORT_DATA/GEOLOCATIONS/phase_angle"))
-    cf     <- data.frame(cloud_fraction_L2 = ncvar_get(t_data, "PRODUCT/SUPPORT_DATA/INPUT_DATA/cloud_fraction_L2"))
-    
-    # Transform to vect for clipping to ROI
-    ndvi_v     <- vect(cbind(lon, lat), atts = ndvi, crs = "+proj=longlat +datum=WGS84")
-    ndvi_roi   <- intersect(ndvi_v, roi)
-    nirv_v     <- vect(cbind(lon, lat), atts = nirv, crs = "+proj=longlat +datum=WGS84")
-    nirv_roi   <- intersect(nirv_v, roi)
-    nirv_r_v   <- vect(cbind(lon, lat), atts = nirv_r, crs = "+proj=longlat +datum=WGS84")
-    nirv_r_roi <- intersect(nirv_r_v, roi)
-    sif_v      <- vect(cbind(lon, lat), atts = sif, crs = "+proj=longlat +datum=WGS84")
-    sif_roi    <- intersect(sif_v, roi)
-    sif_d_v    <- vect(cbind(lon, lat), atts = sif_d, crs = "+proj=longlat +datum=WGS84")
-    sif_d_roi  <- intersect(sif_d_v, roi)
-    sif_e_v    <- vect(cbind(lon, lat), atts = sif_e, crs = "+proj=longlat +datum=WGS84")
-    sif_e_roi  <- intersect(sif_e_v, roi)
-    rad_v      <- vect(cbind(lon, lat), atts = rad, crs = "+proj=longlat +datum=WGS84")
-    rad_roi    <- intersect(rad_v, roi)
-    rad_v      <- vect(cbind(lon, lat), atts = rad, crs = "+proj=longlat +datum=WGS84")
-    rad_roi    <- intersect(rad_v, roi)
-    pa_v       <- vect(cbind(lon, lat), atts = pa, crs = "+proj=longlat +datum=WGS84")
-    pa_roi     <- intersect(pa_v, roi)
-    cf_v       <- vect(cbind(lon, lat), atts = cf, crs = "+proj=longlat +datum=WGS84")
-    cf_roi     <- intersect(cf_v, roi)
-    
-    # Construct df of all values
+  time_s <- Sys.time()
+  print(paste0("Working on: ", input_file))
+
+  t_data <- nc_open(input_file)
+  
+  # Get spatial and time
+  lon <- ncvar_get(t_data, "PRODUCT/longitude")
+  lat <- ncvar_get(t_data, "PRODUCT/latitude")
+  t   <- basename(input_file)
+  t   <- substr(t, 14, 23)
+
+  # Get variables
+  ndvi   <- data.frame(NDVI = ncvar_get(t_data, "PRODUCT/NDVI"))
+  nirv   <- data.frame(NIRv = ncvar_get(t_data, "PRODUCT/NIRv"))
+  nirv_r <- data.frame(NIRv_RAD = ncvar_get(t_data, "PRODUCT/NIRv_RAD"))
+  sif    <- data.frame(SIF_743 = ncvar_get(t_data, "PRODUCT/SIF_743"))
+  sif_d  <- data.frame(SIF_Corr_743 = ncvar_get(t_data, "PRODUCT/SIF_Corr_743"))
+  sif_e  <- data.frame(SIF_ERROR_743 = ncvar_get(t_data, "PRODUCT/SIF_ERROR_743"))
+  rad    <- data.frame(Mean_TOA_RAD_743 = ncvar_get(t_data, "PRODUCT/SUPPORT_DATA/DETAILED_RESULTS/Mean_TOA_RAD_743"))
+  pa     <- data.frame(phase_angle = ncvar_get(t_data, "PRODUCT/SUPPORT_DATA/GEOLOCATIONS/phase_angle"))
+  cf     <- data.frame(cloud_fraction_L2 = ncvar_get(t_data, "PRODUCT/SUPPORT_DATA/INPUT_DATA/cloud_fraction_L2"))
+  
+  # Transform to vect for clipping to ROI
+  v          <- vect(cbind(lon, lat), atts = ndvi, crs = "+proj=longlat +datum=WGS84")
+  ndvi_roi   <- intersect(v, roi)
+
+  # If number of soundings > 0, then proceed
+  if (nrow(crds(ndvi_roi, df = TRUE)) == 0) {
+    print(paste0("File for this date is being skipped as it has 0 soundings for the region: ", t))
+    print("")
+  } else {
     df <- crds(ndvi_roi, df = TRUE)
     df <- cbind(df, ndvi_roi[[1]])
+    
+    print(paste0("Number of soundings for this file is: ", length(df$NDVI)))
+
+    v          <- vect(cbind(lon, lat), atts = nirv, crs = "+proj=longlat +datum=WGS84")
+    nirv_roi   <- intersect(v, roi)
+    v          <- vect(cbind(lon, lat), atts = nirv_r, crs = "+proj=longlat +datum=WGS84")
+    nirv_r_roi <- intersect(v, roi)
+    v          <- vect(cbind(lon, lat), atts = sif, crs = "+proj=longlat +datum=WGS84")
+    sif_roi    <- intersect(v, roi)
+    v          <- vect(cbind(lon, lat), atts = sif_d, crs = "+proj=longlat +datum=WGS84")
+    sif_d_roi  <- intersect(v, roi)
+    v          <- vect(cbind(lon, lat), atts = sif_e, crs = "+proj=longlat +datum=WGS84")
+    sif_e_roi  <- intersect(v, roi)
+    v          <- vect(cbind(lon, lat), atts = rad, crs = "+proj=longlat +datum=WGS84")
+    rad_roi    <- intersect(v, roi)
+    v          <- vect(cbind(lon, lat), atts = pa, crs = "+proj=longlat +datum=WGS84")
+    pa_roi     <- intersect(v, roi)
+    v          <- vect(cbind(lon, lat), atts = cf, crs = "+proj=longlat +datum=WGS84")
+    cf_roi     <- intersect(v, roi)
+    
+    v          <- c() # kick out of memory
+    
+    # Construct df of all values
     df <- cbind(df, nirv_roi[[1]])
     df <- cbind(df, nirv_r_roi[[1]])
     df <- cbind(df, sif_roi[[1]])
@@ -81,7 +111,7 @@ clip_TROPOSIF <- function (in_dir, roi_file, out_dir){
     ###
     
     # Create dimensions nc file
-    elemdim <- ncdim_def("n_elem", "", seq(1,length(df$NDVI)))
+    elemdim <- ncdim_def("n_elem", "", seq(1, length(df$NDVI)))
     
     t_num   <- as.numeric(julian(as.Date(t), origin = as.Date("1970-01-01")))
     # timedim <- ncdim_def("time", "days since 1970-01-01", t_num)
@@ -148,9 +178,6 @@ clip_TROPOSIF <- function (in_dir, roi_file, out_dir){
     ncatt_put(ncout,0,"references", "http://ftp.sron.nl/open-access-data-2/TROPOMI/tropomi/sif/v2.1/l2b/)")
     ncatt_put(ncout,0,"date_created", date())
     
-    # Get a summary of the created file:
-    # ncout
-    
     # Close input file
     nc_close(ncout)
     nc_close(t_data)
@@ -158,9 +185,10 @@ clip_TROPOSIF <- function (in_dir, roi_file, out_dir){
     time_e <- Sys.time()
     time_dif <- difftime(time_e, time_s)
     print(paste0("Saved ", out_f, ". Time elapsed: ", time_dif))
-    print(paste0("Estimated time remaining: ", format(time_dif * (length(f_list) - i))), units = "hours")
-    
+    print("")
   }
+  
+  tmp_remove(tmpdir)
 }
 
-clip_TROPOSIF(in_dir, roi_file, out_dir)
+mclapply(f_list, clip_TROPOSIF, mc.cores = 4, mc.preschedule = FALSE, roi_file = roi_file, out_dir = out_dir, tmpdir = tmpdir)
